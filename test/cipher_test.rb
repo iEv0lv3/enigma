d178ee8eeac06f7d5b5b8c4199d9cee70d1ab090
Jordan <jordan.wallace.williams@gmail.com>
2020-01-15 01:24:44 -0700

    Add integration tests for encrypt and decrypt message methods

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -10,7 +10,7 @@ class CipherTest < Minitest::Test
   def test_cipher_parse_message
     message = 'wonderful little message!'
 
-    expected = ["w", "o", "n", "d", "e", "r", "f", "u", "l", " ", "l", "i", "t", "t", "l", "e", " ", "m", "e", "s", "s", "a", "g", "e", "!"]
+    expected = ['w', 'o', 'n', 'd', 'e', 'r', 'f', 'u', 'l', ' ', 'l', 'i', 't', 't', 'l', 'e', ' ', 'm', 'e', 's', 's', 'a', 'g', 'e', '!']
 
     assert_equal expected, parse_message(message)
   end
@@ -19,12 +19,30 @@ class CipherTest < Minitest::Test
     message = 'wonderful little message!'
     message_split = parse_message(message)
 
-    expected = [22, 14, 13, 3, 4, 17, 5, 20, 11, 26, 11, 8, 19, 19, 11, 4, 26, 12, 4, 18, 18, 0, 6, 4, "!"]
+    expected = [22, 14, 13, 3, 4, 17, 5, 20, 11, 26, 11, 8, 19, 19, 11, 4, 26, 12, 4, 18, 18, 0, 6, 4, '!']
 
     assert_equal expected, message_alphabet_indexes(message_split)
   end
 
   def test_encrypt_message
-    # encrypt_message integration test
+    new_key = Key.new('04039')
+    new_date = Offset.new('140122')
+    message = 'wonderful little message!'
+    shift = create_shift(new_key, new_date)
+
+    expected = 'diytmlqjtuwyanwuhgph vru!'
+
+    assert_equal expected, encrypt_message(message, shift)
+  end
+
+  def test_decrypt_message
+    new_key = Key.new('04039')
+    new_date = Offset.new('140122')
+    message = 'diytmlqjtuwyanwuhgph vru!'
+    shift = create_shift(new_key, new_date)
+
+    expected = 'wonderful little message!'
+
+    assert_equal expected, decrypt_message(message, shift)
   end
 end
