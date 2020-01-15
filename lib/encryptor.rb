f31243955a2cff987b9ab3530fc0c8d60f823e05
Jordan <jordan.wallace.williams@gmail.com>
2020-01-15 03:15:52 -0700

    Add and update encryptor subclass for encrypting srp

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,7 +1,24 @@
 require_relative 'cryptor'
+require 'date'
+require './lib/modules/shift'
+require './lib/modules/cipher'
+require_relative 'key'
+require_relative 'offset'
 
 class Encryptor < Cryptor
-  def initialize(text, key = '', date = '')
-    super(text, key, date)
+  attr_reader :secret
+
+  include Shift
+  include Cipher
+
+  def initialize(message, key = '', date = '')
+    @secret = ''
+    super(message, key, date)
+  end
+
+  def build_crypt
+    downcase_message = @message.downcase
+    shift = create_shift(@key, @offset)
+    @secret = encrypt_message(downcase_message, shift)
   end
 end
