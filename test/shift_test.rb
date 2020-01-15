c93e04744856016fa716e1c373e72da5e07f29fd
Jordan <jordan.wallace.williams@gmail.com>
2020-01-15 01:13:55 -0700

    Add integration tests for apply_shift method

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -12,7 +12,7 @@ class ShiftTest < Minitest::Test
   include Shift
   include Cipher
 
-  def test_keys_and_offsets_can_be_added
+  def test_keys_and_offsets_can_be_combined
     key = Key.new
     key.stubs(:digits).returns('34781')
     key.make_keys(key.digits)
@@ -38,6 +38,29 @@ class ShiftTest < Minitest::Test
     assert_equal expected, create_shift_sets(indexes)
   end
 
+  def test_apply_shift
+    new_key = Key.new('04039')
+    new_date = Offset.new('140122')
+    message = 'wonderful little message!'
+    encrypted_message = 'diytmlqjtuwyanwuhgph vru!'
+
+    message_split = parse_message(message)
+    shift = create_shift(new_key, new_date)
+    indexes = message_alphabet_indexes(message_split)
+
+    expected = 'diytmlqjtuwyanwuhgph vru!'
+
+    assert_equal expected, apply_shift(indexes, shift, 'encrypt')
+
+    message_split = parse_message(encrypted_message)
+    shift = create_shift(new_key, new_date)
+    indexes = message_alphabet_indexes(message_split)
+
+    expected2 = 'wonderful little message!'
+
+    assert_equal expected2, apply_shift(indexes, shift, 'decrypt')
+  end
+
   def test_shift_can_be_added_to_shift_sets
     new_key = Key.new('04039')
     new_date = Offset.new('140122')
@@ -48,7 +71,7 @@ class ShiftTest < Minitest::Test
     indexes = message_alphabet_indexes(message_split)
     shift_sets = create_shift_sets(indexes)
 
-    expected = "diytmlqjtuwyanwuhgph vru!"
+    expected = 'diytmlqjtuwyanwuhgph vru!'
 
     assert_equal expected, add_shift_to_indexes(shift_sets, shift)
   end
@@ -56,7 +79,7 @@ class ShiftTest < Minitest::Test
   def test_shift_can_be_removed_from_shift_sets
     new_key = Key.new('04039')
     new_date = Offset.new('140122')
-    message = "diytmlqjtuwyanwuhgph vru!"
+    message = 'diytmlqjtuwyanwuhgph vru!'
 
     message_split = parse_message(message)
     shift = create_shift(new_key, new_date)
